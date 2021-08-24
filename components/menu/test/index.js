7208732b83a9586b923c7189e81a890066dde27b
Aaron Hans <aaronhans@gmail.com>
2021-08-24 16:46:30 -0700

    clarify code comments

--- components/menu/test/index.js
+++ components/menu/test/index.js
@@ -27,16 +27,16 @@ describe('<cagov-navoverlay>', () => {
     // reproduce these results manually by installing the axe chrome extension and loading this component's preview.html file after running npm run start in the repo root to start local web server (https://chrome.google.com/webstore/detail/axe-devtools-web-accessib/lhdoppojpmngadmnindnejefpokejbdd/related?hl=en-US)
     await expect(el).to.be.accessible(); // vscode thinks this await is unnecessary but it is required when doing the accessible test
 
-    // verify an expected initial class
+    // verify an expected initial class is present
     expect(el.querySelector('.search-container')).to.have.class('hidden-search'); 
     
-    // verify class expected only to be added on click is not there
+    // verify attribute expected to change on click is in the initial off state
     expect(el.querySelector('.expanded-menu-col').getAttribute('aria-expanded')).to.equal('false');    
 
     // click to expand menu
     el.querySelector('.expanded-menu-col').click();
 
-    // verify class added after click is there
+    // verify attribute is changed as expected after click
     expect(el.querySelector('.expanded-menu-col').getAttribute('aria-expanded')).to.equal('true');
 
   });
