7e9848302b5ba743928430cd2806718f94b27d3e
Jon Jensen <github@id.xjensen.com>
2021-10-20 15:30:54 -0700

    Clean up eslintrc

--- components/menu/.eslintrc.cjs
+++ components/menu/.eslintrc.cjs
@@ -1,16 +1,14 @@
 module.exports = {
-	settings: {
-		"import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
-	},
-	parserOptions: {
-		sourceType: "module",
-		allowImportExportEverywhere: true,
-        ecmaVersion: 2017
-    },
-	env: {
-    	es6: true
-    },
-	plugins: [
-		"html"
-	]
-}
+  settings: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    ecmaVersion: 2017,
+  },
+  env: {
+    es6: true,
+  },
+  plugins: ['html'],
+};
