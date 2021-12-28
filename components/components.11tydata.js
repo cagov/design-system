a32ae64728d3c0b22d4d361b94d713e8c1f184ae
Jon Jensen <github@id.xjensen.com>
2021-12-27 16:53:41 -0800

    Infrastucture for component lists

--- components/components.11tydata.js
+++ components/components.11tydata.js
@@ -1,4 +1,4 @@
 module.exports = {
   layout: 'component-page',
-  tags: 'component',
+  tags: ['component'],
 };
---
+++ docs/site/_data/components.js
@@ -0,0 +1,118 @@
+module.exports = {
+  structural: [
+    {
+      name: 'Statewide header',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'statewide-header',
+    },
+    {
+      name: 'Site header',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'branding',
+    },
+    {
+      name: 'Statewide footer',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'statewide-footer',
+    },
+    {
+      name: 'Site footer',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'agency-footer',
+    },
+    {
+      name: 'Page feedback',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'feedback',
+    },
+  ],
+  navigation: [
+    {
+      name: 'Back to top button',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'back-to-top',
+    },
+    {
+      name: 'Link icon',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'link-icon',
+    },
+    {
+      name: 'Page navigation',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'content-navigation',
+    },
+    {
+      name: 'Pagination',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'pagination',
+    },
+    {
+      name: 'Site navigation',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'menu',
+    },
+    {
+      name: 'Skip to content',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'skip-to-content',
+    },
+  ],
+  visual: [
+    {
+      name: 'Base CSS',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'base-css',
+    },
+  ],
+  content: [
+    {
+      name: 'Accordion',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'accordion',
+    },
+    {
+      name: 'Link grid',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'button-grid',
+    },
+    {
+      name: 'Feature card',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'feature-card',
+    },
+    {
+      name: 'Page alert',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'page-alert',
+    },
+    {
+      name: 'Regulatory outline',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'regulatory-outline',
+    },
+    {
+      name: 'Step list',
+      status: 'Alpha',
+      blurb: 'Blurb.',
+      slug: 'step-list',
+    },
+  ],
+};
