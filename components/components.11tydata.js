46e3728f8e8c3bcee414226d591fdc7bdc20d1f6
Jon Jensen <github@id.xjensen.com>
2021-12-29 16:34:00 -0800

    Component page menu and dev stage

--- components/components.11tydata.js
+++ components/components.11tydata.js
@@ -1,4 +1,8 @@
 module.exports = {
   layout: 'component-page',
   tags: ['component'],
+  eleventyComputed: {
+    componentSlug: (article) =>
+      article.page.filePathStem.match(/\/components\/(.+?)\/.+/)[1],
+  },
 };
