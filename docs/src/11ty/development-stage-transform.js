57172d7ab60770209150368a644288b5904a31cf
Jon Jensen <github@id.xjensen.com>
2022-01-04 11:38:18 -0800

    Set up for dev stage transform

---
+++ docs/src/11ty/development-stage-transform.js
@@ -0,0 +1,13 @@
+module.exports = (html, outputPath) => {
+  let result = html;
+
+  // outputPath === false means serverless templates
+  if (!outputPath || outputPath.endsWith(".html")) {
+    // Render post-lists
+    if (html.includes("cagov-post-list")) {
+      result = html;
+    }
+  }
+
+  return result;
+}
