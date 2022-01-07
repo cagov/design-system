7af8bb62ca7313393ca982d472c3b73f69868426
Chach Sikes <chachsikes@ChachSikes.local>
2022-01-06 17:15:38 -0800

    Add shell of airtable form component

---
+++ components/airtable-form/src/index.js
@@ -0,0 +1,15 @@
+export class CaGovAirtableForm extends window.HTMLElement {
+    // .env variable
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      
+    }
+  
+
+  }
+  
+  window.customElements.define('cagov-airtable-form', CaGovAirtableForm);
+  
