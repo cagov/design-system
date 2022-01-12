a5d1441b8ecb5306e42612e5dfc7b8a2373dc14e
Aaron Hans <aaronhans@gmail.com>
2022-01-12 11:16:06 -0800

    fix lint issue

--- docs/src/publish/cdn-deploy.js
+++ docs/src/publish/cdn-deploy.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const AWS = require('aws-sdk');
+
 const s3 = new AWS.S3();
 
 // publish to  S3 bucket at /components/${package-name}/v${package-version}/
-let currentPackageInfo = JSON.parse(fs.readFileSync('./package.json','utf8'));
+const currentPackageInfo = JSON.parse(fs.readFileSync('./package.json','utf8'));
 const packageName = currentPackageInfo.name.replace('@cagov/','');
 const packageVersion = currentPackageInfo.version;
 const keyPrefix = `components/${packageName}/v${packageVersion}/dist/`;
 
-const directoryToUpload = process.cwd()+'/dist';
+const directoryToUpload = `${process.cwd()}/dist`;
 const bucketName = 'cdn.designsystem.webstandards.ca.gov';
 
 // get file paths
 const filePaths = [];
 const getFilePaths = (dir) => {
-  fs.readdirSync(dir).forEach(function (name) {
+  fs.readdirSync(dir).forEach((name) => {
     const filePath = path.join(dir, name);
     const stat = fs.statSync(filePath);
     if (stat.isFile()) {
@@ -29,27 +30,25 @@ const getFilePaths = (dir) => {
 getFilePaths(directoryToUpload);
 
 // upload to S3
-const uploadToS3 = (dir, path) => {
-  return new Promise((resolve, reject) => {
-    const key = keyPrefix + path.split(`${dir}/`)[1];
+const uploadToS3 = (dir, filePath) => new Promise((resolve, reject) => {
+    const key = keyPrefix + filePath.split(`${dir}/`)[1];
     const params = {
       Bucket: bucketName,
       Key: key,
-      Body: fs.readFileSync(path),
+      Body: fs.readFileSync(filePath),
     };
     s3.putObject(params, (err) => {
       if (err) {
         reject(err);
       } else {
         console.log(`uploaded ${params.Key} to ${params.Bucket}`);
-        resolve(path);
+        resolve(filePath);
       }
     });
   });
-};
 
-const uploadPromises = filePaths.map((path) =>
-  uploadToS3(directoryToUpload, path)
+const uploadPromises = filePaths.map((filePath) =>
+  uploadToS3(directoryToUpload, filePath)
 );
 Promise.all(uploadPromises)
   .then((result) => {
