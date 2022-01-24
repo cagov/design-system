7a0c845b1e62019d7c9156a5f9c9ba51d2145ba0
Aaron Hans <aaronhans@gmail.com>
2022-01-23 21:43:37 -0800

    put css in js in page alert and synchronize test markupa nd readme markkup, add pagination directions

--- components/page-alert/test/index.test.js
+++ components/page-alert/test/index.test.js
@@ -1,18 +1,20 @@
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture } from '@open-wc/testing';
 /* this test file can be run on command line with npm run test
    or with visual debug via npm run test:visual */
 
-import '../src/index.js';
+import '../dist/index.js';
 
 describe('Page Alert', function unitTest() {
   this.timeout(20000);
   it('works', async () => {
-    const el = await fixture(
-      html`<cagov-page-alert
-        data-icon="ca-gov-icon-bell"
-        data-message='Notice: DCC is soliciting proposals for the Local Jurisdiction Assistance Grant Program. &gt;a href="/about-us/grant-funding/local-jurisdiction-assistance-grant-program/"&lt;Learn more&gt;/a&lt;.'
-      ></cagov-page-alert>`,
-    );
+
+    const response = await fetch('../template.html');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const startHTML = await response.text();
+    const el = await fixture(startHTML);
 
     // click if icon has aria-hidden attribute
     await expect(
