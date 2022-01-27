680d2994d96e3eb83803c5db905059c4252551c8
Konstantin Koryaka <konstantin@konstantinsimac.attlocal.net>
2022-01-27 12:14:09 -0800

    js formatting

--- components/content-navigation/src/index.js
+++ components/content-navigation/src/index.js
@@ -10,8 +10,6 @@ import styles from '../index.css';
  * @attr {string} [data-label] - "On this page";
  */
 
-
-
 class CAGovContentNavigation extends window.HTMLElement {
   connectedCallback() {
     this.type = 'wordpress';
@@ -44,59 +42,59 @@ class CAGovContentNavigation extends window.HTMLElement {
               : Date.now;
           var c =
             ((l = o.navigator.userAgent),
-              new RegExp(['MSIE ', 'Trident/', 'Edge/'].join('|')).test(l)
-                ? 1
-                : 0);
+            new RegExp(['MSIE ', 'Trident/', 'Edge/'].join('|')).test(l)
+              ? 1
+              : 0);
           (o.scroll = o.scrollTo =
             function () {
               void 0 !== arguments[0] &&
                 (!0 !== f(arguments[0])
                   ? h.call(
-                    o,
-                    t.body,
-                    void 0 !== arguments[0].left
-                      ? ~~arguments[0].left
-                      : o.scrollX || o.pageXOffset,
-                    void 0 !== arguments[0].top
-                      ? ~~arguments[0].top
-                      : o.scrollY || o.pageYOffset,
-                  )
+                      o,
+                      t.body,
+                      void 0 !== arguments[0].left
+                        ? ~~arguments[0].left
+                        : o.scrollX || o.pageXOffset,
+                      void 0 !== arguments[0].top
+                        ? ~~arguments[0].top
+                        : o.scrollY || o.pageYOffset,
+                    )
                   : i.scroll.call(
-                    o,
-                    void 0 !== arguments[0].left
-                      ? arguments[0].left
-                      : typeof arguments[0] !== 'object'
+                      o,
+                      void 0 !== arguments[0].left
+                        ? arguments[0].left
+                        : typeof arguments[0] !== 'object'
                         ? arguments[0]
                         : o.scrollX || o.pageXOffset,
-                    void 0 !== arguments[0].top
-                      ? arguments[0].top
-                      : void 0 !== arguments[1]
+                      void 0 !== arguments[0].top
+                        ? arguments[0].top
+                        : void 0 !== arguments[1]
                         ? arguments[1]
                         : o.scrollY || o.pageYOffset,
-                  ));
+                    ));
             }),
             (o.scrollBy = function () {
               void 0 !== arguments[0] &&
                 (f(arguments[0])
                   ? i.scrollBy.call(
-                    o,
-                    void 0 !== arguments[0].left
-                      ? arguments[0].left
-                      : typeof arguments[0] !== 'object'
+                      o,
+                      void 0 !== arguments[0].left
+                        ? arguments[0].left
+                        : typeof arguments[0] !== 'object'
                         ? arguments[0]
                         : 0,
-                    void 0 !== arguments[0].top
-                      ? arguments[0].top
-                      : void 0 !== arguments[1]
+                      void 0 !== arguments[0].top
+                        ? arguments[0].top
+                        : void 0 !== arguments[1]
                         ? arguments[1]
                         : 0,
-                  )
+                    )
                   : h.call(
-                    o,
-                    t.body,
-                    ~~arguments[0].left + (o.scrollX || o.pageXOffset),
-                    ~~arguments[0].top + (o.scrollY || o.pageYOffset),
-                  ));
+                      o,
+                      t.body,
+                      ~~arguments[0].left + (o.scrollX || o.pageXOffset),
+                      ~~arguments[0].top + (o.scrollY || o.pageYOffset),
+                    ));
             }),
             (e.prototype.scroll = e.prototype.scrollTo =
               function () {
@@ -121,13 +119,13 @@ class CAGovContentNavigation extends window.HTMLElement {
                       void 0 !== arguments[0].left
                         ? ~~arguments[0].left
                         : typeof arguments[0] !== 'object'
-                          ? ~~arguments[0]
-                          : this.scrollLeft,
+                        ? ~~arguments[0]
+                        : this.scrollLeft,
                       void 0 !== arguments[0].top
                         ? ~~arguments[0].top
                         : void 0 !== arguments[1]
-                          ? ~~arguments[1]
-                          : this.scrollTop,
+                        ? ~~arguments[1]
+                        : this.scrollTop,
                     );
                   }
               }),
@@ -135,19 +133,19 @@ class CAGovContentNavigation extends window.HTMLElement {
               void 0 !== arguments[0] &&
                 (!0 !== f(arguments[0])
                   ? this.scroll({
-                    left: ~~arguments[0].left + this.scrollLeft,
-                    top: ~~arguments[0].top + this.scrollTop,
-                    behavior: arguments[0].behavior,
-                  })
+                      left: ~~arguments[0].left + this.scrollLeft,
+                      top: ~~arguments[0].top + this.scrollTop,
+                      behavior: arguments[0].behavior,
+                    })
                   : i.elementScroll.call(
-                    this,
-                    void 0 !== arguments[0].left
-                      ? ~~arguments[0].left + this.scrollLeft
-                      : ~~arguments[0] + this.scrollLeft,
-                    void 0 !== arguments[0].top
-                      ? ~~arguments[0].top + this.scrollTop
-                      : ~~arguments[1] + this.scrollTop,
-                  ));
+                      this,
+                      void 0 !== arguments[0].left
+                        ? ~~arguments[0].left + this.scrollLeft
+                        : ~~arguments[0] + this.scrollLeft,
+                      void 0 !== arguments[0].top
+                        ? ~~arguments[0].top + this.scrollTop
+                        : ~~arguments[1] + this.scrollTop,
+                    ));
             }),
             (e.prototype.scrollIntoView = function () {
               if (!0 !== f(arguments[0])) {
@@ -156,7 +154,7 @@ class CAGovContentNavigation extends window.HTMLElement {
                     ;
                     o !== t.body &&
                     !1 ===
-                    ((e = p((l = o), 'Y') && a(l, 'Y')),
+                      ((e = p((l = o), 'Y') && a(l, 'Y')),
                       (r = p(l, 'X') && a(l, 'X')),
                       e || r);
 
@@ -171,22 +169,22 @@ class CAGovContentNavigation extends window.HTMLElement {
                 const r = this.getBoundingClientRect();
                 l !== t.body
                   ? (h.call(
-                    this,
-                    l,
-                    l.scrollLeft + r.left - e.left,
-                    l.scrollTop + r.top - e.top,
-                  ),
+                      this,
+                      l,
+                      l.scrollLeft + r.left - e.left,
+                      l.scrollTop + r.top - e.top,
+                    ),
                     o.getComputedStyle(l).position !== 'fixed' &&
-                    o.scrollBy({
-                      left: e.left,
-                      top: e.top,
-                      behavior: 'smooth',
-                    }))
+                      o.scrollBy({
+                        left: e.left,
+                        top: e.top,
+                        behavior: 'smooth',
+                      }))
                   : o.scrollBy({
-                    left: r.left,
-                    top: r.top,
-                    behavior: 'smooth',
-                  });
+                      left: r.left,
+                      top: r.top,
+                      behavior: 'smooth',
+                    });
               } else
                 i.scrollIntoView.call(
                   this,
@@ -215,8 +213,8 @@ class CAGovContentNavigation extends window.HTMLElement {
           return t === 'Y'
             ? o.clientHeight + c < o.scrollHeight
             : t === 'X'
-              ? o.clientWidth + c < o.scrollWidth
-              : void 0;
+            ? o.clientWidth + c < o.scrollWidth
+            : void 0;
         }
         function a(t, l) {
           const e = o.getComputedStyle(t, null)[`overflow${l}`];
@@ -416,8 +414,6 @@ if (customElements.get('cagov-content-navigation') === undefined) {
   );
 }
 
-
-
 const style = document.createElement('style');
 style.textContent = styles;
 document.querySelector('head').appendChild(style);
@@ -434,7 +430,7 @@ function mobileView() {
 function insertAfter(referenceNode, newNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
-const contentnav = document.querySelector(".sidebar-container");
+const contentnav = document.querySelector('.sidebar-container');
 const h1Header = document.querySelector('h1');
 
 if (mobileView()) {
@@ -442,4 +438,3 @@ if (mobileView()) {
     insertAfter(h1Header, contentnav);
   }
 }
-
