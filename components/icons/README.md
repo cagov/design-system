8a9a02b09dd1eff25b7a8303ad7decfad909dfd4
zakiya <zakiya@users.noreply.github.com>
2022-02-08 18:52:44 -0800

    608 - icons - Allow command line users to generate a subset of icons.

--- components/icons/README.md
+++ components/icons/README.md
@@ -3,4 +3,4 @@
 - `npm run sprite -ids=accessible arrow cough` : Compile CSS.
 - `-ids` : A space-separated list of icon ids.
 
-For example: `npm run sprite -ids=accessible arrow cough`
+For example: `npm run sprite -ids=accessibility arrow-down left`
--- components/icons/scripts/sprite.js
+++ components/icons/scripts/sprite.js
@@ -1,7 +1,9 @@
 // import { fs } from fs;
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import { exec } from 'child_process';
 
+// Set up arguments for the command line.
 const { argv } = yargs(hideBin(process.argv))
   .option('ids', {
     alias: 'i',
@@ -11,7 +13,22 @@ const { argv } = yargs(hideBin(process.argv))
   .help()
   .alias('help', 'h');
 
+// Parse arguments and run through svg-generate command.
 if (argv.ids) {
-  console.log('Generating the following:');
+  // Inputs and outputs.
+  const inputStart = 'dist/svg/';
+  const inputEnd = `.svg`;
+  const inputSeparator = `${inputEnd},${inputStart}`;
+  const input = inputStart + argv.ids.join(inputSeparator) + inputEnd;
+  const output = 'cagov-select-sprite.svg';
+
+  // Run command.
+  const cmd = `svg-sprite-generate -l ${input} -o ${output}`;
+
+  // Report.
+  console.log('Attempting to combine SVGs...');
   console.log(argv.ids);
+  console.log(`...from ${inputStart} to ${output} `);
+
+  exec(cmd);
 }
