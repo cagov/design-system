0c5045698491343d13f21eefd172131ee22fa29c
zakiya <zakiya@users.noreply.github.com>
2022-02-16 14:19:50 -0800

    608 - icons - Sequence individual icon building steps.

--- components/icons/scripts/generateIcons.js
+++ components/icons/scripts/generateIcons.js
@@ -1,19 +1,28 @@
 import wget from 'node-wget';
 import fontBlast from 'font-blast';
+import util from 'util';
 import vars from './vars.js';
 
 // svg-sprite-generate -d dist/svg -o dist/allicons.svg",
 
+const urlToSVG = util.promisify(wget);
+
 const generateIcons = async () => {
-  const promise1 = () => {
-    wget({ url: vars.cagovUrl, dest: vars.componentDirFonts });
-  };
+  urlToSVG({ url: vars.cagovUrl, dest: vars.componentDirFonts });
+  console.log(
+    `==> CAGOV: ${vars.cagovUrl} ==> ${vars.componentDirFonts}CaGov.svg`,
+  );
 
   const promise2 = () => {
     fontBlast(vars.componentFontAll, vars.componentDir);
+    console.log(
+      `==> CAGOV: ${vars.componentFontAll} ==> ${vars.componentDir}/*.svg`,
+    );
   };
 
-  promise1();
+  urlToSVG({ url: vars.cagovUrl, dest: vars.componentDirFonts })
+    .then(() => promise2())
+    .catch((err) => console.log(err.message));
 };
 
 generateIcons();
